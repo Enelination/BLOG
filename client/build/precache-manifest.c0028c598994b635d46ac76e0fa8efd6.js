9a7f555db4e1734b00189a566c3f277e0afb8557
enel <enel@gmail.com>
2021-02-20 00:17:31 +0000

    v1.0.0

---
+++ client/build/precache-manifest.c0028c598994b635d46ac76e0fa8efd6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1e82935655200c5d6f6e8ac2c6156b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23f652960f1ca10f0d2a",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7884973085db5848f54a",
+    "url": "./static/css/main.5803987b.chunk.css"
+  },
+  {
+    "revision": "23f652960f1ca10f0d2a",
+    "url": "./static/js/2.1eb9a426.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "./static/js/2.1eb9a426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7884973085db5848f54a",
+    "url": "./static/js/main.713a78de.chunk.js"
+  },
+  {
+    "revision": "27bc183d629a888a1e51",
+    "url": "./static/js/runtime-main.9a5e869b.js"
+  },
+  {
+    "revision": "66adbeab59a36d78ce927ec507a8a593",
+    "url": "./static/media/blog.66adbeab.svg"
+  },
+  {
+    "revision": "630c11e4d4febac4cc08e67cd2213d29",
+    "url": "./static/media/date.630c11e4.svg"
+  },
+  {
+    "revision": "e8fef7e5b142469d5ee6fed872f0ee5a",
+    "url": "./static/media/heart.e8fef7e5.svg"
+  },
+  {
+    "revision": "14ce8ba42bf0e7f712acfc35a23f68d7",
+    "url": "./static/media/liked.14ce8ba4.svg"
+  },
+  {
+    "revision": "d1e3d0c158177513cad5544491770059",
+    "url": "./static/media/not-liked.d1e3d0c1.svg"
+  }
+]);
